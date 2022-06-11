e82f1d081d41c6ff27546637535eb23726254e57
Parker <event.roleplay.1@mail.ru>
2022-06-11 15:10:44 +0300

    Init

---
+++ config.js
@@ -0,0 +1,13 @@
+module.exports = {
+ "verification": "",
+ "description": "",
+ "domain": "https://localhost", // This Domain need to be without https:// or http:// (eg: hydrabot.fun not https://hydrabot.fun)
+ "google_analitics": "", 
+ "token": "OTcwODE3NjMzODUzMjQ3NTY4.G1vg3R.aFC3i2QYCyX8LJQvRevA_OhZXVWZ23UgebQeAM",
+ "https":"https:/localhost",
+ "port":"5003",
+ "client_id":"970817633853247568",
+ "secret":"UDwjcJg2B2QqTk7-aaligRj8_tDHY_4w",
+ "support_server_link":"https://discord.gg/uHP6dPQp4C",
+ "bot_name":"LA STORIES"
+}
